3d46293d2b7a4f2a0da0cf5c218b6a4a455b26d6
bojanatesic <tesic.bojana@yahoo.com>
2022-01-08 23:56:52 +0100

    added if statements

--- dices/index.js
+++ dices/index.js
@@ -3,11 +3,19 @@ var heading = document.querySelector(".title");
 var imageOne = document.querySelector(".img1");
 var imageTwo = document.querySelector(".img2");
 
-var pictures = ["images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png"]
+var pictures = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"]
 
-var randomNumber1 = Math.floor(Math.random()* 5) ;
+var randomNumber1 = Math.floor(Math.random() * 5);
 imageOne.src = pictures[randomNumber1]
 
-var randomNumber2 = Math.floor(Math.random()* 5) ;
+var randomNumber2 = Math.floor(Math.random() * 5);
 imageTwo.src = pictures[randomNumber2]
 
+if (imageOne.src < imageTwo.src) {
+    heading.innerHTML = "Player 2 wins!"
+}
+if (imageOne.src > imageTwo.src) {
+    heading.innerHTML = "Player 1 wins!"
+} else if (imageOne.src == imageTwo.src) {
+    heading.innerHTML = "Draw!"
+}
